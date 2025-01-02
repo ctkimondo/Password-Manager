be5d6d2377ebc92ef148efaf0ba66168d579522e
ctkimondo <catuki17@gmail.com>
2025-01-02 15:14:31 -0500

    Modified

---
+++ app.py
@@ -0,0 +1,106 @@
+import psycopg
+import os
+from dotenv import load_dotenv
+import pmfunctions
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Get the database credentials from the environment variables
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+master_password = os.getenv("MASTER_PASSWORD")
+
+# Create a PasswordManager object
+pm = pmfunctions.PasswordManager(master_password)
+
+try:
+    # Connect to the default database
+    conn = psycopg.connect(dbname='postgres', user=db_user, password=db_password, host=db_host, port=db_port)
+
+    # Create a cursor to execute queries
+    cursor= conn.cursor()
+
+    # Check if the database exists
+    cursor.execute("SELECT 1 FROM pg_database WHERE datname = 'passwordmanager'")
+    exists = cursor.fetchone()
+
+    if not exists:
+        # Create a new database
+        cursor.execute('''CREATE DATABASE passwordmanager''')
+        
+        # Confirm the creation of the database
+        print("Database created successfully")
+
+        # Close the cursor and the connection to the default database
+        cursor.close()
+        conn.close()
+
+        conn.commit()
+
+        # Connect to the recently created database
+        conn = psycopg.connect(dbname = 'passwordmanager', user=db_user, password=db_password, host=db_host, port=db_port)
+        
+        # Create a cursor to execute queries
+        cursor = conn.cursor()
+
+        # Create a table to store the passwords
+        cursor.execute('''CREATE TABLE IF NOT EXISTS passwords(
+                    id SERIAL PRIMARY KEY,
+                    username VARCHAR(255) NOT NULL,
+                    password VARCHAR(255) NOT NULL,
+                    site_name VARCHAR(255) NOT NULL
+                        )''')
+        
+        # Confirm the creation of the table
+        print("Table created")
+
+        # Close the cursor and the connection to the database
+        cursor.close()
+        conn.close()
+
+        conn.commit()
+
+# Handle exceptions
+except Exception as e:
+    print("Error: {}", e)
+
+# Create a menu
+while (True):
+    print("What would you like to do?")
+    print("================================\n")
+    print("1. Add a new password\n")
+    print("2. Retrieve a password\n")
+    print("3. Delete a password\n")
+    print("4. Exit\n")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        # Get the site name, username and password
+        site_name = input("Enter the site name:")
+        username = input("Enter your site username: ")
+        password = input("Enter your desired password: ")
+
+        # Add the password
+        pm.add_password(site_name, username, password)
+    
+    elif choice == '2':
+        # Get the site name
+        site_name = input("Enter the site name:")
+
+        # Retrieve the password
+        pm.retrieve_password(site_name)
+    
+    elif choice == '3':
+        # Get the site name
+        site_name = input("Enter the site name:")
+
+        # Delete the password
+        pm.delete_password(site_name)
+    
+    elif choice == '4':
+        print("Exiting the program")
+        break
---
+++ pmfunctions.py
@@ -0,0 +1,168 @@
+import psycopg
+import os
+from dotenv import load_dotenv
+import cryptography
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Get the database credentials from the environment variables
+db_name = os.getenv("DB_NAME")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+class PasswordManager:
+    def __init__(self, master_password):
+        # Setting the backend to handle cryptographic operations, defaulting to OpenSSL
+        self.backend = default_backend()
+        
+        # Generate a random salt
+        self.salt = os.urandom(16)
+
+        # Generate a key from the master password
+        self.encryption_key = self.derive_key(master_password, self.salt)
+    
+    def derive_key(self, master_password, salt):
+        # Derive a key from the salted master password
+        kdf = PBKDF2HMAC(
+            algorithm=SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=self.backend
+        )
+
+        # Return the derived key
+        return kdf.derive(master_password.encode())
+    
+    def encrypt(self, plaintext):
+        # Generate a random initialization vector to ensure that the same plaintext
+        # does not encrypt the same ciphertext
+        iv = os.urandom(16)
+
+        # Create a cipher object using the encryption key, the AES algo and the CFB mode
+        cipher = Cipher(algorithms.AES(self.encryption_key), modes.CFB(iv), backend=self.backend)
+
+        # Create an encryptor object
+        encryptor = cipher.encryptor()
+
+        # Pad the plaintext to ensure that it is a multiple of 16 bytes
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(plaintext.encode()) + padder.finalize()
+
+        # Encrypt the padded data
+        ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+
+        # Return the initialization vector and the ciphertext
+        return urlsafe_b64encode(iv + ciphertext).decode()
+    
+    def decrypt(self, encrypted_text):
+        # Decode the encoded text
+        decoded_text = urlsafe_b64decode(encrypted_text.encode())
+
+        # Extract the iv (first 16 bytes)
+        iv = decoded_text[:16]
+
+        # Extract the ciphertext (remaining bytes after the iv)
+        ciphertext = decoded_text[16:]
+
+        # Create a cipher object using the encryption key, the AES algo and the CFB mode
+        cipher = Cipher(algorithms.AES(self.encryption_key), modes.CFB(iv), backend=self.backend)
+
+        # Create a decryptor object
+        decryptor = cipher.decryptor()
+
+        # Decyrpt the ciphertext to retrieve the padded plaintext
+        padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+        # Initialze an unpadder to remove the padding
+        unpadder = padding.PKCS7(128).unpadder()
+
+        # Unpad the plaintext
+        plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
+
+        # Decode the plaintext to a string and return it
+        return plaintext.decode()
+    
+    def add_password(self, site_name, username, password):
+        # Connect to the database
+        conn = psycopg.connect(dbname = 'passwordmanager',user=db_user, password=db_user, host=db_host, port=db_port)
+
+        # Create a cursor to execure queries
+        cursor = conn.cursor()
+
+        # Encrypt the password
+        encrypted_password = self.encrypt(password)
+
+        # Insert the password into the table while using parameterized queries to prevent SQL injections
+        query = '''INSERT INTO passwords (username, password, site_name)
+                VALUES (%s, %s, %s)'''
+        data = (username, encrypted_password, site_name)
+        cursor.execute(query, data)
+
+        # Confirm the insertion of the password
+        print("Password added successfully")
+
+        # Close the cursor and the connection to the database
+        cursor.close()
+        conn.close()
+
+        conn.commit()
+    
+    def retrieve_password(self, site_name):
+        # Connect to the database
+        conn = psycopg.connect(dbname='passwordmanager',user=db_user, password=db_user, host=db_host, port=db_port)
+
+        # Create a cursor to execure queries
+        cursor = conn.cursor()
+
+        # Select the password from the table using the site name
+        # while using parameterized queries to prevent SQL injections
+        query = '''SELECT password FROM passwords WHERE site_name = %s'''
+        data = (site_name)
+        cursor.execute(query, data)
+
+        # Fetch the password
+        password = cursor.fetchone()[0]
+
+        # Decrypt the password
+        decrypted_password = self.decrypt(password)
+
+        # Confirm the retrival of the password
+        print("Password retrieved successfully !! = %s", decrypted_password)
+
+        # Close the cursor and the connection to the database
+        cursor.close()
+        conn.close()
+
+        conn.commit()
+
+    def delete_password(self, site_name):
+        # Connect to the database
+        conn = psycopg.connect(dbname='passwordmanager',user=db_user, password=db_user, host=db_host, port=db_port)
+
+        # Create a cursor to execure queries
+        cursor = conn.cursor()
+
+        # Delete the password from the table 
+        # while using parameterized queries to prevent SQL injections
+        query = '''DELETE FROM passwords WHERE site_name = %s'''
+        data = (site_name)
+        cursor.execute(query, data)
+
+        # Confirm the deletion of the password
+        print("Password deleted successfully")
+
+        # Close the cursor and the connection to the database
+        cursor.close()
+        conn.close()
+
+        conn.commit()
